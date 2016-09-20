5d3aa20b10135056ddf1c45f13785fc3bb8ed81f
Patrick Cullen <patrick.cullen@washpost.com>
2016-09-20 15:35:05 -0400

    init cloud-compose

---
+++ cloud-compose/templates/datadog.zookeeper.sh
@@ -0,0 +1,13 @@
+# datadog.zookeeper.sh 
+cat <<- 'EOF' > /etc/dd-agent/conf.d/zk.yaml
+init_config:
+
+instances:
+    -   host: localhost
+        port: 2181
+        timeout: 3
+EOF
+# datadog.start
+sh -c "sed 's/api_key:.*/api_key: {{DATADOG_API_KEY}}/' /etc/dd-agent/datadog.conf.example > /etc/dd-agent/datadog.conf"
+echo 'bind_host: 0.0.0.0' >> /etc/dd-agent/datadog.conf
+service datadog-agent restart
